1d860dd2544d67007dce6dfce7c1a665d05f3d91
alexeusgr <alexeusgr@gmail.com>
2022-07-30 15:32:11 +0300

    tested conf

--- configuration.nix
+++ configuration.nix
@@ -162,10 +162,10 @@
   nix = {
     binaryCaches          = [ "https://hydra.iohk.io" "https://iohk.cachix.org" ];
     binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" ];
-package = pkgs.nixFlakes; # or versioned attributes like nixVersions.nix_2_8
+    package = pkgs.nixFlakes; # or versioned attributes like nixVersions.nix_2_8
     extraOptions = ''
-	keep-outputs = true
-        keep-derivations = true
+	    keep-outputs = true
+      keep-derivations = true
     	experimental-features = nix-command flakes
     '';
   };
