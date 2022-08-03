8e37273ca5dccc0302b292fe3004356fe4511a1a
alexeusgr <alexeusgr@gmail.com>
2022-08-03 19:21:20 +0300

    fix conf.nix

--- configuration.nix
+++ configuration.nix
@@ -79,10 +79,9 @@
     programs.bash = {
     enable = true;
     profileExtra = ''
-      if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
-          __GIT_PROMPT_DIR=$(brew --prefix)/opt/bash-git-prompt/share
-          GIT_PROMPT_ONLY_IN_REPO=1
-          source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
+      if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+         GIT_PROMPT_ONLY_IN_REPO=1
+         source $HOME/.bash-git-prompt/gitprompt.sh
       fi
     '';
   };
